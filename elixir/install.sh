f07d48f2f38d0341cf22dc23d7237fd62d2c74a4
Pierrot <pierre@gros-pc.(none)>
2013-12-13 04:21:10 +0100

    Added Elixir - refs #1

---
+++ elixir/install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# See http://elixir-lang.org/getting_started/1.html
+
+install_remote_deb() {
+  local FILE=`mktemp`
+  local URL=$1
+
+  wget "$URL" -O $FILE
+  sudo dpkg -i $FILE
+  sudo apt-get -f install # needed in case the package has unmet dependencies
+
+  rm -f $FILE
+}
+
+install_elixir() {
+  local SOURCES=$ZSH/elixir/sources
+
+  # [todo] - Add an option to update Elixir if it is already installed
+  if test ! $(which elixir)
+  then
+    echo "  Installing elixir for you."
+    git clone -b stable https://github.com/elixir-lang/elixir.git $SOURCES
+    cd $SOURCES
+    make clean test
+  fi
+}
+
+if test ! $(which erl)
+then
+  echo "  Installing erlang for you."
+  install_remote_deb "http://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_16.b.3-1~ubuntu~precise_amd64.deb"
+fi
+
+install_elixir
---
+++ elixir/path.zsh
@@ -0,0 +1,1 @@
+export PATH="$PATH:$ZSH/elixir/sources/bin"
